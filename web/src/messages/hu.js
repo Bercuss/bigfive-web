fd2679284b2ae2e2e1563328ebc9be508b30d6df
FKWBKB <szepesibertalan@stud.uni-obuda.hu>
2026-02-05 00:43:27 +0100

    nyelv

--- web/src/messages/hu.js
+++ web/src/messages/hu.js
@@ -21,12 +21,63 @@ export default {
     // ...további kulcsok
   },
   frontpage: {
-    title: 'Főoldal',
-    description: 'Üdvözlünk a Swan személyiségteszt oldalán! Töltsd ki a Big Five tesztet, és ismerd meg jobban önmagad.'
+    seo: {
+      title: 'Swan személyiségelemző teszt',
+      description: ''
+    },
+    title: 'Big Five személyiségteszt',
+    call_to_action: 'Töltsd ki az ingyenes tesztet most',
+    no_registration: '* Nem szükséges regisztráció',
+    tests_taken: 'Több mint <green>{n}</green> kitöltés',
+    compare: {
+      title: 'Hasonlítsd össze magad másokkal',
+      text1: 'Hasonlítsd össze magad a pároddal, kollégáiddal, barátaiddal vagy családoddal.',
+      text2: 'Tudd meg, miben különbözöl másoktól az öt fő személyiségdimenzióban:',
+      action: 'Nézd meg élőben'
+    },
+    share: 'Oszd meg barátaiddal, és tudd meg, mennyire vagytok kompatibilisek',
+    cards: {
+      open: {
+        title: 'Nyílt',
+        text: 'Ez egy nyílt forráskódú projekt MIT licenc alatt.'
+      },
+      free: {
+        title: 'Ingyenes',
+        text: 'A teszt teljesen ingyenes.'
+      },
+      scientific: {
+        title: 'Tudományos',
+        text: 'A BigFive egy tudományosan validált és megbízható pszichológiai modell.'
+      },
+      translated: {
+        title: 'Lefordítva',
+        text: 'Több mint 20 nyelvre lefordítva. Segíts a <a href="https://b5.translations.alheimsins.net/" rel="noreferrer" target="blank">fordítási oldalon</a>!'
+      }
+    },
+    description: {
+      top: 'Ismerd meg jobban önmagad egy <violet>ingyenes</violet>, nyílt forráskódú személyiségteszttel.',
+      subtop: 'A',
+      subtop2: ' a legmegbízhatóbb és leginkább tudományosan validált pszichológiai modell a személyiség mérésére.',
+      reference: 'A teszt és az értékelés ehhez az oldalhoz az <a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a> oldalról származik, az inventory pedig <i>Johnson (2014) 120-item IPIP NEO-PI-R</i>.',
+      info: 'A teszt 120 kérdést tartalmaz, kitöltése kb. 10 percet vesz igénybe.',
+      result: 'A teszt kitöltése után részletes személyiségjelentést kapsz az alábbi dimenziókban:',
+      tests_taken: 'eddig kitöltött tesztek száma'
+    }
   },
   facets: 'Faktorok',
-  getResult: 'Eredmény lekérése',
+  getResult: {
+    result: 'Eredmény',
+    explanation: 'Ha kitöltötted a tesztet és elmentetted az azonosítót, itt megtekintheted az eredményt az azonosító megadásával.',
+    idInput: 'Azonosító mezőbe írd be',
+    urlOrId: 'URL vagy azonosító',
+    getResult: 'Eredmény lekérése',
+    viewPrevious: 'Előző eredmény megtekintése'
+  },
   about: {
+    seo: {
+      title: 'Rólunk és a Swan csapat',
+      description: 'A Swan személyiségteszt mögött egy aktív és elkötelezett csapat áll, akik folyamatosan fejlesztik a tesztet.'
+    },
     title: 'Rólunk',
     description: 'Tudj meg többet a Swan személyiségtesztről és a Big Five modellről.'
   }
