52692de76ee8ecd6757296e48daa55dba440100f
FKWBKB <szepesibertalan@stud.uni-obuda.hu>
2026-02-06 00:27:55 +0100

    vercel build fix

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,27 @@
+const path = require('path');
 const createNextIntlPlugin = require('next-intl/plugin');
 const { withContentlayer } = require('next-contentlayer');
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config) => {
+		config.resolve = config.resolve || {};
+		config.resolve.alias = config.resolve.alias || {};
+
+		// Ensure monorepo package is resolvable during build (including on Vercel)
+		config.resolve.alias['@bigfive-org/score'] = path.join(
+			__dirname,
+			'..',
+			'packages',
+			'score',
+			'build',
+			'src'
+		);
+
+		return config;
+	}
+};
 
 module.exports = withContentlayer(withNextIntl(nextConfig));
